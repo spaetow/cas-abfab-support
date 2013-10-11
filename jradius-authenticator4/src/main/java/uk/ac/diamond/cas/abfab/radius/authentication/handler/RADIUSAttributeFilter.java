041515b7e2be0b1a06e39c4bc182457df177bd94
Stefan Paetow <stefan.paetow@diamond.ac.uk>
2013-10-11 17:30:39 +0100

    Changes to the attribute filters (gives us more flex to derive and adjust).

---
+++ jradius-authenticator4/src/main/java/uk/ac/diamond/cas/abfab/radius/authentication/handler/RADIUSAttributeFilter.java
@@ -0,0 +1,37 @@
+package uk.ac.diamond.cas.abfab.radius.authentication.handler;
+
+import net.jradius.exception.UnknownAttributeException;
+import net.jradius.packet.attribute.AttributeFactory;
+import net.jradius.packet.attribute.AttributeList;
+import net.jradius.packet.attribute.RadiusAttribute;
+import net.jradius.packet.attribute.value.StringValue;
+
+public class RADIUSAttributeFilter {
+    
+    /** The attributes used to initialise this class */
+    private AttributeList attributes;
+
+    /**
+     * @param attributes A JRadius AttributeList to filter
+     */
+    public RADIUSAttributeFilter(
+            final AttributeList attributes) {
+        this.attributes = attributes;
+    }
+    
+    /**
+     * @param attributeName A string representing the name of the attribute
+     * @return Object array representing the attribute
+     */
+    public Object[] getAttribute(final String attributeName) throws UnknownAttributeException {
+        final Object[] attributeArray = this.attributes.getArray(AttributeFactory.getTypeByName(attributeName));
+        return attributeArray;
+    }
+    
+    /**
+     * @return The RADIUS attributes stored in this class
+     */
+    public AttributeList getAttributes() {
+        return attributes;
+    }
+}
--- jradius-authenticator4/src/main/java/uk/ac/diamond/cas/abfab/radius/authentication/handler/SAMLAssertionAttributeFilter.java
+++ jradius-authenticator4/src/main/java/uk/ac/diamond/cas/abfab/radius/authentication/handler/SAMLAssertionAttributeFilter.java
@@ -6,28 +6,25 @@ import net.jradius.packet.attribute.AttributeList;
 import net.jradius.packet.attribute.RadiusAttribute;
 import net.jradius.packet.attribute.value.StringValue;
 
-public class SAMLAssertionAttributeFilter {
+public class SAMLAssertionAttributeFilter extends RADIUSAttributeFilter {
     
     /** The SAML Assertion Attribute name in RADIUS */
     private final static String attributeName = "SAML-AAA-Assertion";
     
-    /** The attributes used to initialise this class */
-    private AttributeList attributes;
-
     /**
      * @param attributes A JRadius AttributeList to filter
      */
     public SAMLAssertionAttributeFilter(
             final AttributeList attributes) {
-        this.attributes = attributes;
+       super(attributes);
     }
     
     /**
-     * @return A string representing the complete attribute
+     * @return A string representing the complete attribute concatenated from all SAML-AAA-Assertion attributes
      */
     public String getAssertion() throws UnknownAttributeException {
         StringBuilder sb = new StringBuilder();
-        final Object[] attributeArray = this.attributes.getArray(AttributeFactory.getTypeByName(attributeName));
+        final Object[] attributeArray = this.getAttribute(attributeName);
         for (final Object attributeObject : attributeArray) {
             if (attributeObject instanceof RadiusAttribute) {
                 final StringValue sv = (StringValue) ((RadiusAttribute) attributeObject).getValue();
@@ -35,12 +32,5 @@ public class SAMLAssertionAttributeFilter {
             }
         }
         return sb.toString();
-    }
-    
-    /**
-     * @return The RADIUS attributes stored in this class
-     */
-    public AttributeList getAttributes() {
-        return attributes;
     }
 }
